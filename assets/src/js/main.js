9a467199de0bc3d5d8cf07801268577ce30e71ed
Rafael Fernandes <rafatexfr@gmail.com>
2017-03-02 10:49:06 +0100

    First sort of production ready thingie
    Removed service workers, connectivity warnings, changed author and page-contacts
    Will eventually take care of those later

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -99,25 +99,25 @@
   	//*** Service Worker (PWA Cache)
 
     //* Register the service worker if available.
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js').then(function(reg) {
-            console.log('Successfully registered service worker', reg);
-        }).catch(function(err) {
-            console.warn('Error whilst registering service worker', err);
-        });
-    }
+    // if ('serviceWorker' in navigator) {
+    //     navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    //         console.log('Successfully registered service worker', reg);
+    //     }).catch(function(err) {
+    //         console.warn('Error whilst registering service worker', err);
+    //     });
+    // }
 
     window.addEventListener('online', function(e) {
         // Resync data with server.
         console.log("You are online");
-        Page.hideOfflineWarning();
+        // Page.hideOfflineWarning();
         //Arrivals.loadData();
     }, false);
 
     window.addEventListener('offline', function(e) {
         // Queue up events for server.
         console.log("You are offline");
-        Page.showOfflineWarning();
+        // Page.showOfflineWarning();
     }, false);
 
 	////
